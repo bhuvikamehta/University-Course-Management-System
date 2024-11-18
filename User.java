275e06e8235d3b9b15ad713d5182725f5d5a4e48
bhuvikamehta <146580397+bhuvikamehta@users.noreply.github.com>
2024-11-19 01:48:16 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+abstract class User {
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void displayMenu();
+}
